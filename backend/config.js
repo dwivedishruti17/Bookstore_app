356233b3b61ed974c1c988c6c4f1829819710a80
dwivedishruti17 <134783858+dwivedishruti17@users.noreply.github.com>
2024-05-18 00:40:25 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5000;
+
+export const mongoDBURL =
+  'mongodb+srv://shrutimun17:RRlkSCT11J6KY2fB@cluster0.ldcblry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
